5ea1779ca2b0c93a895adbeedd2fd974a6be4286
kelly r maass <kmaass77627@gmail.com>
2020-02-01 20:39:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
   end 
 
   get '/square/:number' do
-  "#{params[:number].to_i.square!}"
+  "#{params[:number].to_i.**2}"
   end  
   #@num = params[:number]**2
  # @num.to_i 
