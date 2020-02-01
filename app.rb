f7150200a2c8c9e01cf0a0df694c08fb79d4a6bf
kelly r maass <kmaass77627@gmail.com>
2020-02-01 19:58:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,11 +3,13 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
-get '/reversename/:name' do 
-  params[:name].reverse 
-end 
+  get '/reversename/:name' do 
+    params[:name].reverse 
+  end 
 
-get 
+  get '/square/:number'
+  params[:number]
+
 
 
 
