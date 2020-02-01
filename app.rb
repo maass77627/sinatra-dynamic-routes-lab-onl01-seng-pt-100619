7619df43c1c2932003a0194edeb020e222adb478
kelly r maass <kmaass77627@gmail.com>
2020-02-01 20:55:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
   end 
   
   get '/say/:number/:phrase' do 
-  params[:number].to_i.times { puts params[:phrase] }
+   "#{params[:number].to_i.times { puts params[:phrase] }} "
  end 
 
 
